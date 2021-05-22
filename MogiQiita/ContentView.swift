728aeab7697c2e66b21e65d7d96fb70288053e09
大谷悦志 <324etsushi@gmail.com>
2021-05-22 14:09:50 +0900

    first commit

---
+++ MogiQiita/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MogiQiita
+//
+//  Created by 大谷悦志 on 2021/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MogiQiita/MogiQiitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MogiQiitaApp.swift
+//  MogiQiita
+//
+//  Created by 大谷悦志 on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MogiQiitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
