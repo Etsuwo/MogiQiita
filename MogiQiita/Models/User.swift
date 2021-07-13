a844aa0a5d305d9806c368bbcb16f8734589b1c0
大谷悦志 <324etsushi@gmail.com>
2021-07-13 22:01:02 +0900

    Add new files

---
+++ MogiQiita/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  MogiQiita
+//
+//  Created by 大谷悦志 on 2021/07/08.
+//
+
+import Foundation
---
+++ MogiQiita/URLImageView.swift
@@ -0,0 +1,20 @@
+//
+//  URLImageView.swift
+//  MogiQiita
+//
+//  Created by 大谷悦志 on 2021/07/13.
+//
+
+import SwiftUI
+
+struct URLImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct URLImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        URLImageView()
+    }
+}
